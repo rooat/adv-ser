ca7b091f2cdcd846a98b37be282c455e10bf8d79
rooat <welling1234@gmail.com>
2019-09-26 11:25:12 +0800

    u

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 var axios = require('axios');
 function te1(){
-    axios.post("/get_adv_one",{"ad_id":1}).then(function(res){
+    axios.post("http://localhost:3000/get_adv_one",{"ad_id":1}).then(function(res){
         console.log("res:",res.data.resp)
     })
 }
 function te2(){
-    axios.post("/get_adv_list",{}).then(function(res){
+    axios.post("http://localhost:3000/get_adv_list",{}).then(function(res){
         console.log("res:",res.data.resp)
     })
 }
